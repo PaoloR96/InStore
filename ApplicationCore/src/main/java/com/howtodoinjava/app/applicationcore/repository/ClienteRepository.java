ea63a03cdf7d30cc0c9fea57d10ccab98e508cd4
biagi <biagio.salzillo@studenti.unicampania.it>
2025-01-22 17:42:09 +0100

    campi privati

--- ApplicationCore/src/main/java/com/howtodoinjava/app/applicationcore/repository/ClienteRepository.java
+++ ApplicationCore/src/main/java/com/howtodoinjava/app/applicationcore/repository/ClienteRepository.java
@@ -1,13 +1,9 @@
 package com.howtodoinjava.app.applicationcore.repository;
 
-<<<<<<<< HEAD:ApplicationCore/src/main/java/com/howtodoinjava/app/applicationcore/repository/ClienteRepository.java
-public class ClienteRepository {
-========
-import com.howtodoinjava.app.applicationcore.entity.Sconto;
+import com.howtodoinjava.app.applicationcore.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ScontoRepository extends JpaRepository<Sconto, Integer> {
->>>>>>>> github/master:ApplicationCore/src/main/java/com/howtodoinjava/app/applicationcore/repository/ScontoRepository.java
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
 }
