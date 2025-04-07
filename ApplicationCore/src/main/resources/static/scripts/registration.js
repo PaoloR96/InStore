97655f2b86a0d18a2f13783ea8dd690d9fa8a50b
gae-m <gae.gm98@gmail.com>
2025-04-07 16:31:21 +0200

    Fixes after merge

--- ApplicationCore/src/main/resources/static/scripts/registration.js
+++ ApplicationCore/src/main/resources/static/scripts/registration.js
@@ -18,3 +18,28 @@ function enableFieldset() {
         document.getElementById("rivenditoreAttributes").hidden = true;
     }
 }
+const csrfToken = document.cookie.replace(/(?:(?:^|.*;\s*)XSRF-TOKEN\s*\=\s*([^;]*).*$)|^.*$/, '$1');
+
+function clienteRegistration(){
+    fetch("/api/cliente-registration", {
+        method: "POST",
+        headers: new Headers({
+            "Contert-Type": "application/x-www-form-urlencoded",
+            "X-XSRF-TOKEN": csrfToken
+        }),
+        body: new FormData(document.forms["registration"])
+    })
+    // return true
+}
+
+function rivenditoreRegistration(form){
+    fetch("/api/rivenditore-registration", {
+        method: "POST",
+        headers: new Headers({
+            "Contert-Type": "application/x-www-form-urlencoded",
+            "X-XSRF-TOKEN": csrfToken
+        }),
+        body: new FormData(form)
+    })
+    // return true
+}
