5e2f387f02d657b2356f0a12697e7ac82539a01e
biagi <biagio.salzillo@studenti.unicampania.it>
2025-05-08 17:39:43 +0200

    merge e mitigazione di alcuni threat relativi a mysql e vault

--- ApplicationCore/src/main/java/com/howtodoinjava/app/applicationcore/repository/RivenditoreRepository.java
+++ ApplicationCore/src/main/java/com/howtodoinjava/app/applicationcore/repository/RivenditoreRepository.java
@@ -2,9 +2,11 @@ package com.howtodoinjava.app.applicationcore.repository;
 
 import com.howtodoinjava.app.applicationcore.entity.Rivenditore;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface RivenditoreRepository extends JpaRepository<Rivenditore,String> {
     Optional<Rivenditore> findByUsername(String username);
 }
